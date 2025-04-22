c2558e12471a6ad91cc711c060e8a95285668185
Ebrahim Zulqarni <72365795+ebrahim575@users.noreply.github.com>
2025-04-22 23:47:04 +0100

    changes

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['i.ytimg.com'], // Allow YouTube thumbnail domain
   },
-  env: {
-    // Define the path to the yt-dlp binary for the runtime environment
-    // This might help ensure it's recognized during build/deployment
-    YTDLP_BIN: '/var/task/bin/yt-dlp',
-  },
   /* other config options can go here */
 };
 
